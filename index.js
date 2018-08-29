8ddeb37aab352ccb3879ae0bcc758e9f0823de29
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-29 13:07:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much.";
+
+fetch('https://api.github.com/repos/jquery/jquery/commits').then(res => res.json()).then(json=>console.log(json))
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits');
+xhr.responseType = 'json';
+ 
+xhr.onload = function() {
+  console.log(xhr.response);
+};
+ 
+xhr.onerror = function() {
+  console.log('Booo');
+};
+ 
+xhr.send();
