f6e0f9839f89d2aae5621581f622a4b881879d65
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-29 13:17:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ fetch('https://api.github.com/repos/jquery/jquery/commits').then(res => res.json
 let xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits');
 xhr.responseType = 'json';
- 
+
 xhr.onload = function() {
   console.log(xhr.response);
 };
- 
+
 xhr.onerror = function() {
   console.log('Booo');
 };
- 
+
 xhr.send();
